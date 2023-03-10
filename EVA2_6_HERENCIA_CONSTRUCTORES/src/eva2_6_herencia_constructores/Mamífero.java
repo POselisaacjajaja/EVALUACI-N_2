d2beafa143f31cf29523696e084f6847ece7a6a9
Isaac Guzmán <124391806+POselisaacjajaja@users.noreply.github.com>
2023-03-09 23:12:53 -0600

    Add files via upload

---
+++ EVA2_6_HERENCIA_CONSTRUCTORES/src/eva2_6_herencia_constructores/Mamífero.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eva2_6_herencia_constructores;
+
+/**
+ *
+ * @author isaaczapatto
+ */
+public class Mamífero extends Animal{
+    public Mamífero(){
+        System.out.println("Constructor de mamífero: sangre caliente");
+    }
+    
+    
+    public void tenerPelo(){
+        System.out.println("Tienen pelo");
+    }
+}
